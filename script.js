ece596f5653bcdb7174580fc9cc6d718545a7f91
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-09-11 09:25:34 +0200

    works for analogous+ don't know how to restart

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function readColor() {
   writeToInterface(newColors);
   console.log(changeColors(newColors, harmony));
 }
+
 function writeToInterface(newcolors) {
   newcolors.forEach((elm) => {
     const colorbox = document.createElement("div");
@@ -42,6 +43,7 @@ function writeToInterface(newcolors) {
     parent.appendChild(colorbox);
   });
 }
+
 function getHarmony() {
   if (operator.value === "analogous") {
     return "analogous";
@@ -102,6 +104,7 @@ function setInterval(number, max) {
   }
   return number % max;
 }
+
 function RGBToHex(obj) {
   let r = obj.r;
   let g = obj.g;
@@ -234,58 +237,3 @@ function changeHSLtoRGB(HSLObject) {
   console.log(r, g, b);
   return { r, g, b };
 }
-
-/*
-function changeHSLtoRGB(arrayOfNewColors) {
-  arrayOfNewColors.forEach((elm) => {
-    let h = elm.h;
-    let s = elm.s;
-    let l = elm.l;
-
-    h = h;
-    s = s / 100;
-    l = l / 100;
-
-    let c = (1 - Math.abs(2 * l - 1)) * s,
-      x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
-      m = l - c / 2,
-      r = 0,
-      g = 0,
-      b = 0;
-    if (0 <= h && h < 60) {
-      r = c;
-      g = x;
-      b = 0;
-    } else if (60 <= h && h < 120) {
-      r = x;
-      g = c;
-      b = 0;
-    } else if (120 <= h && h < 180) {
-      r = 0;
-      g = c;
-      b = x;
-    } else if (180 <= h && h < 240) {
-      r = 0;
-      g = x;
-      b = c;
-    } else if (240 <= h && h < 300) {
-      r = x;
-      g = 0;
-      b = c;
-    } else if (300 <= h && h < 360) {
-      r = c;
-      g = 0;
-      b = x;
-    }
-    r = Math.round((r + m) * 255);
-    g = Math.round((g + m) * 255);
-    b = Math.round((b + m) * 255);
-
-    let RGBObject = { r, b, g };
-    arrayOfNewColors = Object.assign({}, RGBObject);
-    console.log(arrayOfNewColors);
-    return arrayOfNewColors;
-  });
-  return arrayOfNewColors;
-}
-*/
